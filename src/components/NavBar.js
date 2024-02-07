ee7ae8f5bd9f5969f7c44d20aa343b7897c72dd5
Phil Wekullo <philwekullo@gmail.com>
2024-02-07 13:32:39 +0300

    Complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,23 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  const linkStyles = {
+    display: "inline-block",
+    margin: "10px 16px",
+    textDecoration: "none",
+    color: "black",
+    fontWeight: '500',
+  };
+
+  return (
+    <div className="navbar">
+			<NavLink to="/" style={linkStyles}>Home</NavLink>
+			<NavLink to="/movies" style={linkStyles}>Movies</NavLink>
+			<NavLink to="/directors" style={linkStyles}>Directors</NavLink>
+			<NavLink to="/actors" style={linkStyles}>Actors</NavLink>
+		</div>
+	);
 }
 
 export default NavBar;
